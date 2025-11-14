1d3efd1dee5603dffa21a6a80573f32c27d2269e
Jayaprakash Jayasingh <jayaprakash.jayasingh@ideas2it.com>
2025-11-14 16:02:06 +0530

    Initial commit

---
+++ src/test/java/com/i2i/job_sphere/JobSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.job_sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
